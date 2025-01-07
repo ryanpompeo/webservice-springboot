9a1e8d8e648058bd0c9b1d5bc007dcf084796089
ryanpompeo <ryanpompeo05@gmail.com>
2025-01-07 00:57:00 -0300

    service layer

---
+++ src/main/java/com/example/ProjetoWebService/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.ProjetoWebService.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ProjetoWebService.entities.User;
+import com.example.ProjetoWebService.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
